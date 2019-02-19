f47fd1fe88c55ecf02d0ec68704ab94492cfad23
Márton Lipécz <mlipecz@gmail.com>
2019-02-19 13:53:31 +0100

    simple create issue test works

--- src/main/java/pageFactory/CreateIssueModal.java
+++ src/main/java/pageFactory/CreateIssueModal.java
@@ -1,14 +1,73 @@
 package pageFactory;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CreateIssueModal {
 
-    @FindBy(xpath="//*[@id='aui-flag-container']//a[@class='issue-created-key issue-link']")
-    WebElement successPopup;
+    WebDriver driver;
+    WebDriverWait wait;
+    WebElement popup;
 
-    public CreateIssueModal() {
+    @FindBy(id="create-issue-submit")
+    WebElement submit;
 
+    @FindBy(id="qf-create-another")
+    WebElement createAnotherCheckbox;
+
+    private static By projectFieldLocator = By.id("project-field");
+    private static By issueTypeFieldLocator = By.id("issuetype-field");
+    private static By summaryFieldLocator = By.id("summary");
+    private static By popupLinkLocator = By.xpath("//*[@id='aui-flag-container']//a[@class='issue-created-key issue-link']");
+    private static By popupDivLocator = By.xpath("//*[@id='aui-flag-container']//descendant::div[contains(@class,'aui-message')]");
+
+    public CreateIssueModal(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, 10);
     }
+
+    public void setProject(String project){
+        WebElement projectField = wait.until(ExpectedConditions.elementToBeClickable(projectFieldLocator));
+        projectField.sendKeys(project);
+        projectField.sendKeys(Keys.ENTER);
+    }
+
+    public void setIssueType(String issueType){
+        WebElement bugSelect = wait.until(ExpectedConditions.elementToBeClickable(issueTypeFieldLocator));
+        bugSelect.sendKeys(issueType);
+        bugSelect.sendKeys(Keys.RETURN);
+    }
+
+    public void setSummary(String summary){
+        WebElement summaryField = wait.until(ExpectedConditions.elementToBeClickable(summaryFieldLocator));
+        summaryField.sendKeys(summary);
+    }
+
+    public void setAssignee(){}
+
+    public void setPriority(String priority){}
+
+    public void addLabel(String label){}
+
+    //clicks it and returns whether it is checked
+    public boolean toggleCreateAnotherCheckbox(){
+        createAnotherCheckbox.click();
+        return createAnotherCheckbox.isSelected();
+    }
+
+    //returns the link's text in the popup, or empty string, if there were no popup
+    public String submit(){
+        submit.click();
+        try {
+            return wait.until(ExpectedConditions.visibilityOfElementLocated(popupDivLocator)).getText();
+        }catch(ElementNotVisibleException e){
+            return "";
+        }
+    }
+
+
 }
--- src/main/java/test/TestCreateIssue.java
+++ src/main/java/test/TestCreateIssue.java
@@ -1,11 +1,18 @@
 package test;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pageFactory.CreateIssueModal;
+import pageFactory.Login;
 
 public class TestCreateIssue {
     private static WebDriver driver;
@@ -17,12 +24,28 @@ public class TestCreateIssue {
 //        options.addArguments("headless");
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
-        driver.get("https://jira.codecool.codecanvas.hu");
+//        driver.get("https://jira.codecool.codecanvas.hu");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        new Login(driver).loginWithDashboard();
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources="/createIssue.csv")
-    void createIssue(){
+    @CsvFileSource(resources="/createIssue.csv", numLinesToSkip = 1)
+    void createIssue(String project, String issueType, String summary){
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(By.id("create_link"))).click();
+        CreateIssueModal modal = new CreateIssueModal(driver);
+        modal.setProject(project);
+        modal.setIssueType(issueType);
+        modal.setSummary(summary);
+        String message = modal.submit();
+        System.out.println(message);
+        assertTrue(message.contains(project));
+    }
+
+    void deleteIssue(String url){
 
     }
 }
